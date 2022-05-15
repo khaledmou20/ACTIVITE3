1c7464645cc2d9459a2f259398876608ee8f723f
khaledmou20 <102072526+khaledmou20@users.noreply.github.com>
2022-05-15 13:00:30 +0100

    Add files via upload

---
+++ exo2.js
@@ -0,0 +1,28 @@
+// Les paramètres du serveur.
+var hostname = 'localhost'; 
+var port = 3000; 
+ 
+// Créer un objet de type Express. 
+var app = express(); 
+ 
+//Créer un objet Router pour implémenter les méthodes. 
+var monRoute = express.Router(); 
+  
+monRoute.route('/authors')
+.post(function(req,res){
+    res.send({message : "Ajoute un auteur a la liste", 
+    "INSPIRE_number":"INSPIRE_1245","affiliation":"Univ",
+    "name":'{"first_name":"lassal","last_name":"boualem"}',
+    "title":"Mosta", methode : req.method});
+})
+
+.put(function(req,res){ 
+    res.send({message : "Mise à jour des informations d'un auteur"+req.params.id});
+})
+// Utiliser le routeur
+app.use(monRoute);  
+ 
+// Démarrer le serveur
+app.listen(port, hostname, function(){
+	console.log("Serveur fonctionne sur http://"+ hostname +":"+port); 
+});
